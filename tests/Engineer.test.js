c97756b5f8a16279dd4684ab34297c796de354ae
kate patch <kate.epatch@gmail.com>
2022-06-27 22:24:49 -0600

    trying to figure out why the getGithub test isn't working.  messing around with it

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -7,14 +7,14 @@ test('github', () => {
     expect(engineer.github).toBe(testGithub);
 });
 
-test('getGithub username', () => {
-    const testGithubName = "Github Name";
-    const engineer = new Engineer("Foo", 3, "fake123@fakermail.gov", testGithubName);
-    expect(engineer.getGithub()).toBe(testGithubName);
+test('getGithub returns username', () => {
+    const testGetGithub = "Github Name";
+    const engineer = new Engineer("Foo", 3, "fake123@fakermail.gov", testGetGithub);
+    expect(engineer.getGithub()).toBe(testGetGithub);
 });
 
-test('getRole', () => {
+test('getRole returns new role', () => {
     const testRole = "Engineer";
     const engineer = new Engineer ("Foo", 3, "fake123@fakermail.gov", "Github Name");
     expect(engineer.getRole()).toBe(testRole);
-})
+});
