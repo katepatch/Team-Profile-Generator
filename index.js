3da244e3cfc08b46e139ad8fae3666ab191f4019
kate patch <kate.epatch@gmail.com>
2022-06-28 16:03:10 -0600

    started index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const employee = new Employee();
+
