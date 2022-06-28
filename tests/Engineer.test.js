ce38a994625399b7bd3e5ec0e80730992028e84b
kate patch <kate.epatch@gmail.com>
2022-06-27 21:53:55 -0600

    added first test for engineer.test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,2 +1,7 @@
 const Engineer = require("../lib/Engineer");
 
+test('github', () => {
+    const testGithub = "Github Name";
+    const engineer = new Engineer(testGithub);
+    expect(engineer.github).toBe(testGithub);
+});
