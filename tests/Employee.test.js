40ca3419919539ee15ef69129a68718fd06879a1
kate patch <kate.epatch@gmail.com>
2022-06-27 21:04:59 -0600

    nevermind that was nothing

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,47 +1,50 @@
 //This line keeps auto popping up i don't know what to do about it.
 //const { default: test } = require('node:test');
-const Employee = require("../lib/Employee");
+const Employee = require('../lib/Employee');
 
-test("Employee class", () => {
-  test("name", () => {
+// test('Employee class', () => {
+//         const testEmployee = new Employee();
+//         expect(typeof(employee)).toBe(testEmployee);
+//     });
+
+test('name', () => {
     const testName = "Penny";
     const employee = new Employee(testName);
     expect(employee.name).toBe(testName);
-  });
+});
 
-  test("id", () => {
+test('id', () => {
     const testId = 3;
     const employee = new Employee("Foo", testId);
     expect(employee.id).toBe(testId);
-  });
+});
 
-  test("email", () => {
+test('email', () => {
     const testEmail = "fake123@fakermail.gov";
     const employee = new Employee("Foo", 3, testEmail);
     expect(employee.email).toBe(testEmail);
-  });
+});
 
-  test("getName returns name", () => {
+test('getName returns name', () => {
     const testGetName = "Penny";
     const employee = new Employee(testGetName);
     expect(employee.getName()).toBe(testGetName);
-  });
+});
 
-  test("getId returns id", () => {
+test('getId returns id', () => {
     const testGetId = 3;
     const employee = new Employee("Foo", testGetId);
     expect(employee.getId()).toBe(testGetId);
-  });
+});
 
-  test("getEmail returns email", () => {
-    const testGetEmail = "fake123@fakermail.gov";
+test('getEmail returns email', () => {
+    const testGetEmail = "fake123@fakermail.gov"
     const employee = new Employee("Foo", 3, testGetEmail);
     expect(employee.getEmail()).toBe(testGetEmail);
-  });
+});
 
-  test("getRole returns role of employee", () => {
+test('getRole returns role of employee', () => {
     const testRole = "Employee";
     const employee = new Employee("Penny", 3, "fake123@fakermail.gov");
     expect(employee.getRole()).toBe(testRole);
-  });
 });
