f3f50d6c4116bb662a30314576fdf1e07806314b
kate patch <kate.epatch@gmail.com>
2022-06-27 22:59:51 -0600

    added intern tests

---
+++ tests/Intern.test.js
@@ -0,0 +1,21 @@
+const { exportAllDeclaration } = require('@babel/types');
+const { default: test } = require('node:test');
+const Intern = require('../lib/Intern');
+
+test('school', () => {
+    const testSchool = "Cool School";
+    const intern = new Intern("Foo", 3, "fake123@fakermail.gov", testSchool);
+    expect(intern.school).toBe(testSchool);
+});
+
+test('getSchool returns school', () => {
+    const testGetSchool = "Cool School";
+    const intern = new Intern("Foo", 3, "fake123@fakermail.gov", testGetSchool);
+    expect(intern.getSchool()).toBe(testGetSchool);
+});
+
+test('getRole return Intern', () => {
+    const testRole = "Intern";
+    const intern = new Intern("Foo", 3, 'fake123@fakermail.gov', "Cool School");
+    expect(intern.getRole()).toBe(testRole);
+});
