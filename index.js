56ac1e1c79ef1e72cde11a9a4ed68fe8646e1864
kate patch <kate.epatch@gmail.com>
2022-06-28 16:08:13 -0600

    added more variables to index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 const employee = new Employee();
 
