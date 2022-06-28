1aab2cc43d56d77ab03ea30b10ebf63bbcdf899f
kate patch <kate.epatch@gmail.com>
2022-06-27 20:07:39 -0600

    updated package.json because it wasn't fully updated

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,6 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee class', () => {
-    describe('getName', () => {
         const employee = new Employee();
         expect(typeof(employee)).toBe('object');
     });
-
-
-})
