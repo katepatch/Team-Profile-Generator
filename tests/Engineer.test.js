298edb31d935380c31f5a383f4f642caaf828c2b
kate patch <kate.epatch@gmail.com>
2022-06-27 22:01:17 -0600

    added more tests to engineer test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,3 +1,4 @@
+const { default: test } = require("node:test");
 const Engineer = require("../lib/Engineer");
 
 test('github', () => {
@@ -5,3 +6,15 @@ test('github', () => {
     const engineer = new Engineer(testGithub);
     expect(engineer.github).toBe(testGithub);
 });
+
+test('getGithub username', () => {
+    const testGithubName = "Github Name";
+    const engineer = new Engineer(testGithubName);
+    expect(engineer.getGithub).toBe(testGithubName);
+});
+
+test('getRole', () => {
+    const testRole = "Engineer";
+    const engineer = new Engineer ("Foo", 3, "fake123@fakermail.gov", "Github Name");
+    expect(engineer.getRole()).toBe(testRole);
+})
