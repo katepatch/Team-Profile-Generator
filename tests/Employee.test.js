9c88cbbdb1f43a2dbf29b075f0944c56e4e6f365
kate patch <kate.epatch@gmail.com>
2022-06-27 20:39:43 -0600

    added tests for name, id, email.  email says it is wrong so I need to fix that

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,5 @@
-//This line keeps popping up i don't know what to do about it.
-//const { default: test } = require('node:test');
+//This line keeps auto popping up i don't know what to do about it.
+const { default: test } = require('node:test');
 const Employee = require('../lib/Employee');
 
 test('Employee class', () => {
@@ -11,4 +11,16 @@ test('name', () => {
     const name = "Penny";
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
-})
+});
+
+test('id', () => {
+    const id = 3;
+    const employee = new Employee(id);
+    expect(employee.id).toBe(id);
+});
+
+test('email' () => {
+    const email = "fake123@fakermail.gov";
+    const employee = new Employee(email);
+    expect(employee.email).toBe(email);
+});
