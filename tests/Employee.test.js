6048608b509cbc04954f58f4f0d83549e82b7bdb
kate patch <kate.epatch@gmail.com>
2022-06-27 20:16:03 -0600

    added jest to package.json to run tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,13 @@
+const { default: test } = require('node:test');
 const Employee = require('../lib/Employee');
 
-describe('Employee class', () => {
+test('Employee class', () => {
         const employee = new Employee();
         expect(typeof(employee)).toBe('object');
     });
+
+test('name', () => {
+    const name = "Penny";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+})
