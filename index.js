878169a1432aa87db36205ab547936e690fc744b
kate patch <kate.epatch@gmail.com>
2022-06-29 15:55:05 -0600

    added info to team.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
-const inquirer = require('inquirer');
-const fs = require('fs');
-
 const questionsManager = [
     {
         type: 'input',
