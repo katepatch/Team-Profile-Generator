a6e8a23a3b1ffe8a80728833ac1d3a964966cda8
kate patch <kate.epatch@gmail.com>
2022-08-25 17:20:18 -0600

    commented out a bunch of previous code to start fresh for now.  might go in and add it back but I added a prompt to start building the team

--- index.js
+++ index.js
@@ -8,15 +8,64 @@ const htmlRenderer = require('../src/htmlRenderer')
 
 const teamMembers = [];
 
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "list", 
+            name: "role",
+            message: "What is this person's role?",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+            type: "input",
+            name: "Name",
+            messasge: "What is the manager's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("please enter manager's name");
+                    return false;
+                    }
+            }
 
-
-class Team {
-    constructor(manager, engineer, intern) {
-        this.manager = manager;
-        this.engineer = engineer;
-        this.intern = intern;
-    }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter Manager's ID",
+            validate: ID => {
+                if (isNaN(ID)) {
+                    console.log("Please enter Manager ID");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter manager's email");
+                    return false;
+                }
+            }
+        }
+    ])
 }
+
+// class Team {
+//     constructor(manager, engineer, intern) {
+//         this.manager = manager;
+//         this.engineer = engineer;
+//         this.intern = intern;
+//     }
+// }
 
 // function createManager() {
 //     inquirer.prompt([
@@ -165,94 +214,94 @@ class Team {
 // init();
 
 
-const managerquestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter Manager name',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Manager ID number',
-        validate: answer => {
-            const pass = answer.match(
-                /^[1-9]\d*$/
-            );
-            if (pass) {
-                return true;
-            }
-            return "Please enter a number greater than zero.";
-        },
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter Manager email',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Please enter Manager office number',        
-    },
-    {
-        type: 'confirm',
-        name: 'addNew',
-        message: 'Add new teammates?',
-        default: false,
-    },
-]
+// const managerquestions = [
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: 'Please enter Manager name',
+//     },
+//     {
+//         type: 'input',
+//         name: 'id',
+//         message: 'Manager ID number',
+//         validate: answer => {
+//             const pass = answer.match(
+//                 /^[1-9]\d*$/
+//             );
+//             if (pass) {
+//                 return true;
+//             }
+//             return "Please enter a number greater than zero.";
+//         },
+//     },
+//     {
+//         type: 'input',
+//         name: 'email',
+//         message: 'Please enter Manager email',
+//     },
+//     {
+//         type: 'input',
+//         name: 'officeNumber',
+//         message: 'Please enter Manager office number',        
+//     },
+//     {
+//         type: 'confirm',
+//         name: 'addNew',
+//         message: 'Add new teammates?',
+//         default: false,
+//     },
+// ]
 
-const employeeQuestions = [
-    {
-        type: 'list',
-        name: 'teammates',
-        message: 'Which type of employee would you like to add?',
-        choices: ['Engineer', 'Intern', 'I have completed my team'],
-        }, 
-        {
-            type: 'input',
-            name: 'Github',
-            message: 'Please enter their Github username',        
-        },
-        {
-            type: 'input',
-            name: 'School',
-            message: 'Where does your Intern go to school?',        
-        },  
+// const employeeQuestions = [
+//     {
+//         type: 'list',
+//         name: 'teammates',
+//         message: 'Which type of employee would you like to add?',
+//         choices: ['Engineer', 'Intern', 'I have completed my team'],
+//         }, 
+//         {
+//             type: 'input',
+//             name: 'Github',
+//             message: 'Please enter their Github username',        
+//         },
+//         {
+//             type: 'input',
+//             name: 'School',
+//             message: 'Where does your Intern go to school?',        
+//         },  
     
-    ]
+//     ]
 
-//    const addTeammate = () => {
-//     inquirer.prompt(employeeQuestions).then((answers) => {
-//         if (answers.teammates === 'Engineer') {
-//             const engineer = new Engineer(answers.name, answers.id, answers.email, answers.Github);
-//             teamMembers.push(engineer);
-//         } else if (answers.teammates === 'Intern') {
-//             const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-//             teamMembers.push(intern);
-//         }
-//         if(answers.add) {
-//             addTeammate();
-//         }
-//         else {
-//             createHtml();
-//         }
+// //    const addTeammate = () => {
+// //     inquirer.prompt(employeeQuestions).then((answers) => {
+// //         if (answers.teammates === 'Engineer') {
+// //             const engineer = new Engineer(answers.name, answers.id, answers.email, answers.Github);
+// //             teamMembers.push(engineer);
+// //         } else if (answers.teammates === 'Intern') {
+// //             const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+// //             teamMembers.push(intern);
+// //         }
+// //         if(answers.add) {
+// //             addTeammate();
+// //         }
+// //         else {
+// //             createHtml();
+// //         }
+// //     })
+// //    }
+
+//    const createTeam = () => {
+//     inquirer.prompt(managerquestions).then((answers) => {
+//         console.log(answers);
+//         // const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+//         // teamMembers.push(manager);
+
+//         // if (answers.add) {
+//         //     addTeammate();
+//         // } else {
+//         //     createHtml();
+//         // }
 //     })
 //    }
 
-   const createTeam = () => {
-    inquirer.prompt(managerquestions).then((answers) => {
-        console.log(answers);
-        // const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-        // teamMembers.push(manager);
-
-        // if (answers.add) {
-        //     addTeammate();
-        // } else {
-        //     createHtml();
-        // }
-    })
-   }
-
-   createTeam();
+//    createTeam();
