9009f98ddb1103dca867904ab2b97eda8eb55e07
kate patch <kate.epatch@gmail.com>
2022-08-27 19:02:15 -0600

    added function to generate the html and add in the team cards.  added module exports so it links up to index.js

--- src/htmlRenderer.js
+++ src/htmlRenderer.js
@@ -81,3 +81,28 @@ renderPage = data => {
     const renderTeam = createTeamHtml(teamCards);
     return renderTeam;
 }
+
+const createTeamHtml = teamCards => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Employee Profiles</title>
+    </head>
+    <body>
+        <header>
+            <h1>Team Members</h1>
+        </header>
+    
+        <section>
+            ${teamCards}
+        </section>
+    </body>
+    </html>
+    `
+}
+
+module.exports = htmlRenderer;
