7f8ef72488df5fdea5da0be65167512df2297d6f
kate patch <kate.epatch@gmail.com>
2022-08-27 18:35:29 -0600

    added a generate manager function to render manager space on html

--- src/htmlRenderer.js
+++ src/htmlRenderer.js
@@ -1,2 +1,17 @@
+const genManager = manager => {
+    return `
+        <div class="manager">
+            <h2>${manager.name}</h2>
+            <h3>Manager</h3>
+        </div>
 
+        <div class="manager-info">
+            <ul class="manager-list">
+                <li>ID: ${manager.id}</li>
+                <li>Email: ${manager.email}</li>
+                <li>Office Number: ${manager.officeNumber}</li>
+            </ul>
+        </div>
+    `
+}
 
