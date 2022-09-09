8853061efe00153a2eb33f678176f8a25f65d79c
kate patch <kate.epatch@gmail.com>
2022-09-09 15:20:22 -0600

    added stylesheet

--- src/htmlRenderer.js
+++ src/htmlRenderer.js
@@ -97,6 +97,7 @@ const createTeamHtml = teamCards => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
         <title>Employee Profiles</title>
     </head>
     <body>
